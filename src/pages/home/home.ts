3cdcfa91eb9da65cbb51a4a0b1d500bfe3bf02d2
Roee Hershko <RoeeH1@verifone.com>
2018-07-24 18:08:26 +0300

    tesT

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,47 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { NavController } from 'ionic-angular';
-
-import pdfjsLib from 'pdfjs-dist'
+import {DocumentViewer, DocumentViewerOptions} from "@ionic-native/document-viewer";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage{
 
-  constructor(public navCtrl: NavController) {
-    var loadingTask = pdfjsLib.getDocument('assets/example.pdf');
-    loadingTask.promise.then(function(pdf) {
-      console.log('PDF loaded');
-      pdfjsLib.workerSrc = 'assets/pdf.worker.js';
+  constructor(public navCtrl: NavController, private document: DocumentViewer) {
 
-      // Fetch the first page
-      var pageNumber = 1;
-      pdf.getPage(pageNumber).then(function(page) {
-        console.log('Page loaded');
-
-        // Prepare canvas using PDF page dimensions
-        var canvas = <HTMLCanvasElement> document.getElementById('the-canvas');
-        var viewport = page.getViewport(canvas.width / page.getViewport(1.0).width);
-
-        var context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        // Render PDF page into canvas context
-        var renderContext = {
-          canvasContext: context,
-          viewport: viewport
-        };
-        var renderTask = page.render(renderContext);
-        renderTask.then(function () {
-          console.log('Page rendered');
-        });
-      });
-    }, function (reason) {
-      // PDF loading error
-      console.error(reason);
-    });
   }
 
+  showDocument() {
+    const options: DocumentViewerOptions = {
+      title: 'My PDF'
+    };
+
+    this.document.viewDocument('assets/example.pdf', 'application/pdf', options)
+  }
 }
